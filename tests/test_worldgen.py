fbf7e1c6a2ab6ad4609f76fbd87dc8ffef35d5bd
Julien <julien.vialdetambel@protonmail.com>
2021-04-14 16:30:43 +0200

    added tests on LargeAmmonia

--- tests/test_worldgen.py
+++ tests/test_worldgen.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 
 from .context import worldgen
-from worldgen.world import World, AsteroidBelt
+from worldgen.world import World, LargeAmmonia, AsteroidBelt
 
 
 @pytest.fixture
@@ -11,57 +11,101 @@ def asteroid_belt():
     return AsteroidBelt()
 
 
+@pytest.fixture
+def large_ammonia():
+    # returns a LargeAmmonia instance
+    return LargeAmmonia()
+
+
 def test_set_density_raises_exception_on_attribute_error(asteroid_belt):
     with pytest.raises(AttributeError):
-        asteroid_belt.density = .0
+        asteroid_belt.density = np.nan
 
 
 def test_set_temperature_raises_exception_on_value_error(asteroid_belt):
     with pytest.raises(ValueError):
-        asteroid_belt.temperature = .0
+        asteroid_belt.temperature = np.nan
 
 
 def test_set_hydrosphere_raises_exception_on_attribute_error(asteroid_belt):
     with pytest.raises(AttributeError):
-        asteroid_belt.hydrosphere = .0
+        asteroid_belt.hydrosphere = np.nan
 
 
 def test_set_volatile_mass_raises_exception_on_attribute_error(asteroid_belt):
     with pytest.raises(AttributeError):
-        asteroid_belt.diameter = .0
+        asteroid_belt.diameter = np.nan
 
 
 def test_set_diameter_raises_exception_on_attribute_error(asteroid_belt):
     with pytest.raises(AttributeError):
-        asteroid_belt.volatile_mass = .0
+        asteroid_belt.volatile_mass = np.nan
+
+
+def test_get_size_is_None(asteroid_belt):
+    assert asteroid_belt.size is None
+
+
+def test_get_size_is_valid(large_ammonia):
+    assert large_ammonia.size == World.Size.LARGE
 
 
 def test_get_core_is_None(asteroid_belt):
     assert asteroid_belt.core is None
 
 
+def test_get_core_is_valid(large_ammonia):
+    assert large_ammonia.core == World.Core.ICY_CORE
+
+
 def test_get_hydrosphere_is_nan(asteroid_belt):
     assert np.isnan(asteroid_belt.hydrosphere)
+
+
+def test_get_hydrosphere_is_valid(large_ammonia):
+    assert (large_ammonia.hydrosphere >= .2 and
+            large_ammonia.hydrosphere <= 1)
 
 
 def test_get_diameter_is_nan(asteroid_belt):
     assert np.isnan(asteroid_belt.diameter)
 
 
+def test_get_atmosphere_is_None(asteroid_belt):
+    assert asteroid_belt.atmosphere is None
+
+
+def test_get_atmosphere_is_valid(large_ammonia):
+    assert large_ammonia.atmosphere == ['He', 'NH3', 'CH4']
+
+
 def test_get_pressure_factor_is_nan(asteroid_belt):
     assert np.isnan(asteroid_belt.pressure_factor)
+
+
+def test_get_pressure_factor_is_valid(large_ammonia):
+    assert large_ammonia.pressure_factor == 5
 
 
 def test_get_greenhouse_factor_is_nan(asteroid_belt):
     assert np.isnan(asteroid_belt.greenhouse_factor)
 
 
+def test_get_greenhouse_factor_is_valid(large_ammonia):
+    assert large_ammonia.greenhouse_factor == .2
+
+
 def test_get_volatile_mass_is_nan(asteroid_belt):
     assert np.isnan(asteroid_belt.volatile_mass)
 
 
-def test_get_absorption_equals_class_variable(asteroid_belt):
-    assert asteroid_belt.absorption == AsteroidBelt._absorption
+def test_get_volatile_mass_is_valid(large_ammonia):
+    assert (large_ammonia.volatile_mass >= .3 and
+            large_ammonia.volatile_mass <= 1.8)
+
+
+def test_get_absorption_is_valid(asteroid_belt):
+    assert asteroid_belt.absorption == .97
 
 
 def test_get_gravity_is_nan(asteroid_belt):
@@ -77,13 +121,13 @@ def test_get_pressure_is_nan(asteroid_belt):
 
 
 def test_get_temperature_is_valid(asteroid_belt):
-    assert (asteroid_belt.temperature >= AsteroidBelt._temperature_range.min and
-            asteroid_belt.temperature <= AsteroidBelt._temperature_range.max)
+    assert (asteroid_belt.temperature >= 140 and
+            asteroid_belt.temperature <= 500)
 
 
 def test_get_blackbody_temperature_is_valid(asteroid_belt):
     assert (asteroid_belt.blackbody_temperature == asteroid_belt.temperature /
-            AsteroidBelt._absorption)
+            .97)
 
 
 def test_get_climate_is_valid(asteroid_belt):
@@ -98,3 +142,13 @@ def test_get_climate_is_valid(asteroid_belt):
 
 def test_get_pressure_category_is_None(asteroid_belt):
     assert asteroid_belt.pressure_category is None
+
+
+def test_get_pressure_category_is_valid(large_ammonia):
+    lower = large_ammonia.pressure_category.value
+    upper = np.inf
+    for item in World.Atmosphere:
+        if item.value > lower:
+            upper = item.value
+    assert (large_ammonia.pressure >= lower and
+            large_ammonia.pressure <= upper)
