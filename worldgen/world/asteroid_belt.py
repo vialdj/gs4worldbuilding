64baf0c8b10bf75f39c70d8b087224fc97865076
Julien <julien.vialdetambel@protonmail.com>
2021-06-20 17:52:52 +0200

    fixed ressource randomization

--- worldgen/world/asteroid_belt.py
+++ worldgen/world/asteroid_belt.py
@@ -10,13 +10,12 @@ class AsteroidBelt(World):
     _ressource_range = Range(World.Ressource.WORTHLESS,
                              World.Ressource.MOTHERLODE)
 
-    @classmethod
-    def random_ressource(cls):
+    def random_ressource(self):
         """sum of a 3d roll times over default worlds Ressource Value Table"""
         ressource_distribution = [.00463, .01389, .02778, .11574, .21296, .25,
                                   .21296, .11574, .02778, .01389, .00463]
-        return random.choices(list(cls.Ressource),
-                              weights=ressource_distribution, k=1)[0]
+        self.ressource = random.choices(list(self.Ressource),
+                                        weights=ressource_distribution, k=1)[0]
 
     def __init__(self):
         super(AsteroidBelt, self).__init__()
