6a87f5d5f02edadc0dc95c1871498c57afab54d9
Julien <julien.vialdetambel@protonmail.com>
2021-08-06 10:00:55 +0200

    Ranged properties dynamically scale as range updates, Ranges on enum type property based on index

--- worldgen/model.py
+++ worldgen/model.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+from enum import Enum
 
 import numpy as np
 
@@ -19,13 +20,37 @@ class Model(object):
         if not rng:
             raise AttributeError('can\'t set attribute, no {}_range found'
                                  .format(prop))
-        if np.isnan(value):
-            raise ValueError('can\'t manually set {} value to nan'
-                             .format(prop))
-        if value < rng.min or value > rng.max:
-            raise ValueError('{} value out of range {}'
-                             .format(prop, rng))
-        setattr(self, '_{}'.format(prop), value)
+        if isinstance(value, Enum):
+            value_idx = list(type(value)).index(value)
+            if (value_idx < list(type(value)).index(rng.min) or
+                value_idx > list(type(value)).index(rng.max)):
+                raise ValueError('{} value out of range {}'
+                                 .format(prop, rng))
+            setattr(self, '_{}'.format(prop), value)
+        else:
+            if np.isnan(value):
+                raise ValueError('can\'t manually set {} value to nan'
+                                 .format(prop))
+            if value < rng.min or value > rng.max:
+                raise ValueError('{} value out of range {}'
+                                 .format(prop, rng))
+            setattr(self, '_{}'.format(prop), (value - rng.min) /
+                                      (rng.max - rng.min))
+
+    def _get_ranged_property(self, prop):
+        """getter for ranged value properties"""
+        rng = getattr(self, '{}_range'.format(prop), None)
+        if not rng:
+            raise AttributeError('can\'t get attribute, no {}_range found'
+                                 .format(prop))
+        value = getattr(self, '_{}'.format(prop))
+        if isinstance(value, Enum):
+            value_idx = list(type(value)).index(value)
+            idx = min(max(value_idx, list(type(value)).index(rng.min)),
+                      list(type(value)).index(rng.max))
+            return list(type(value))[idx]
+        else:
+            return value * (rng.max - rng.min) + rng.min
 
     def __iter__(self):
         """yield property names and values"""
