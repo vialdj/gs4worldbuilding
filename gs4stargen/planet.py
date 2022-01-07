df10afca3fbc4cd093ff817f45eda7748589a914
vialdj <julien.vialdetambel@protonmail.com>
2022-01-07 11:55:00 +0100

    axial_tilt property for planets

--- gs4stargen/planet.py
+++ gs4stargen/planet.py
@@ -38,6 +38,19 @@ class Planet(ABC):
 class InplacePlanet(Planet, ABC):
     """the Planet given orbital parameters as an abstract class"""
 
+    def random_axial_tilt(self) -> None:
+        """Roll 3d over Axial Tilt Tables to define axial tilt"""
+        tilt_roll = roll2d6(-2, continuous=True)
+        table_roll = roll3d6()
+        if table_roll < 17:
+            table = {6: 0, 9: 10, 12: 20, 14: 30, 16: 40}
+        else:
+            table_roll = roll1d6()
+            table = {2: 50, 4: 60, 5: 70, 6: 80}
+        self.axial_tilt = (table[list(filter(lambda x: table_roll <= x,
+                           table.keys()))[0]]
+                           + tilt_roll) * u.deg
+
     def random_retrograde(self) -> None:
         """Roll 3d to define retrograde property"""
         self.retrograde = True if roll3d6() > 13 else False
@@ -55,6 +68,27 @@ class InplacePlanet(Planet, ABC):
             rotation = (initial_roll +
                         self._rotation_modifiers[self.size]) * u.h
         self.rotation = min(rotation, self.rotation_bounds.max)
+
+    @property
+    def axial_tilt(self) -> u.Quantity:
+        """the planet axial tilt in degrees, > 90 if the rotation has the retrograde property"""
+        tilt = self._get_bounded_property('axial_tilt')
+        return (180 - tilt if self.retrograde else tilt) * u.deg
+
+    @property
+    def axial_tilt_bounds(self) -> bounds.QuantityBounds:
+        """axial tilt bounds in degrees"""
+        return bounds.QuantityBounds(0 * u.deg, 90 * u.deg)
+
+    @axial_tilt.setter
+    def axial_tilt(self, value):
+        if not isinstance(value, u.Quantity):
+            raise ValueError('expected quantity type value')
+        if 'angle' not in value.unit.physical_type:
+            raise ValueError('can\'t set axial_tilt to value of'
+                             + ' %s physical type' %
+                             value.unit.physical_type)
+        self._set_bounded_property('axial_tilt', value.to(u.deg))
 
     @property
     def blackbody_temperature(self) -> u.Quantity:
@@ -102,12 +136,17 @@ class InplacePlanet(Planet, ABC):
         self._set_bounded_property('rotation', value.to(u.h))
 
     @property
-    def solar_day(self):
+    def solar_day(self) -> u.Quantity:
         """solar day in standard hours"""
         rotation = -self.rotation if self.retrograde else self.rotation
-        return ((self._orbit.period.to(u.h).value * rotation.to(u.h).value) /
-                (self._orbit.period.to(u.h).value - rotation.to(u.h).value)
-                if rotation != self._orbit.period else np.inf) * u.h
+        return abs((self._orbit.period.to(u.h).value * rotation.to(u.h).value) /
+                   (self._orbit.period.to(u.h).value - rotation.to(u.h).value)
+                   if rotation != self._orbit.period else np.inf) * u.h
+
+    @property
+    def tide_locked(self) -> bool:
+        """tide locked readonly property"""
+        return self.rotation == self._orbit.period
 
     @property
     def moons(self):
